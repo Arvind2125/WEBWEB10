aa3e199c51599d4462ab839c93e4d1c0926efbe6
Arpind <2381040@unai.edu>
2025-04-13 23:34:43 +0700

    inital commit

---
+++ src/app.module.ts
@@ -0,0 +1,51 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { AuthModule } from './auth/auth.module';
+import { postmodule } from './post/post.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<string>('POSTGRES_PORT')
+          ? configService.get<number>('POSTGRES_PORT')
+          : 5432,
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        username: configService.get<string>('POSTGRES_USER'),
+        database: configService.get<string>('POSTGRES_DATABASE'),
+        migrations: ['dist/migrations/*.js'],
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        ssl: true,
+      }),
+    }),
+
+    AuthModule,
+    postmodule,
+    UserModule
+    
+  ],
+  controllers: [AppController],
+  providers: [
+    ConfigService, 
+    JwtService, 
+    {
+    provide : APP_GUARD,
+    useClass : AuthGuard,
+    }, 
+    AppService
+  ],
+})
+export class AppModule {}
